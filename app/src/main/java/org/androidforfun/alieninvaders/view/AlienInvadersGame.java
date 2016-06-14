c623fbee83d6959e361559ce27957b8f77ca26a8
Salvatore D'Angelo <sasadangelo@gmail.com>
2016-06-14 14:50:12 +0200

    Small fixes and build of the project in Android Studio 2.

--- app/src/main/java/org/androidforfun/alieninvaders/view/AlienInvadersGame.java
+++ app/src/main/java/org/androidforfun/alieninvaders/view/AlienInvadersGame.java
@@ -3,11 +3,6 @@ package org.androidforfun.alieninvaders.view;
 import org.androidforfun.alieninvaders.framework.Screen;
 import org.androidforfun.alieninvaders.framework.impl.AndroidGame;
 
-/*
- AlienInvadersGame
-
- This class represents the main activity of the Droids game.
- */
 public class AlienInvadersGame extends AndroidGame {
     @Override
     public Screen getStartScreen() {
